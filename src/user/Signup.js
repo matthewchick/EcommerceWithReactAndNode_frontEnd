04caffdfdaf7f455d5b47a5ef29901064d250fad
Matthew Chick <c.chick@unsw.edu.au>
2019-09-24 10:40:44 +1000

    add .env and config.js

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,12 @@
 //Signup Component
 import React from 'react';
 import Layout from '../core/Layout';
+import { API } from '../config';
 
 const Signup = () => (
     <Layout title="SignUp" description="Signup to Node React E-Commerce App">
-    
+      
+        {API}
     </Layout>
 );
 
