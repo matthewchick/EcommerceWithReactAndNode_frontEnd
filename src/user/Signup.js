46c765f97fa145f492512e5c93a365c4b61fa54a
Matthew Chick <c.chick@unsw.edu.au>
2019-09-24 12:45:25 +1000

    handle success and error

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,12 @@
 //Signup Component
 import React, { useState } from 'react';    //use react hook
+import {Link} from 'react-router-dom';
 import Layout from '../core/Layout';
 import { API } from '../config';
 
 const Signup = () => {
+
+    // javascript functions
     // create a state
     const [values, setValues] = useState({
         name: '',
@@ -13,7 +16,7 @@ const Signup = () => {
         success: false
     });
 
-    const {name, email, password} = values;   //object destructing
+    const {name, email, password, error, success} = values;   //object destructing
 
     const handleChange = name => event => {
         setValues({...values, error: false, [name]: event.target.value})
@@ -21,7 +24,8 @@ const Signup = () => {
 
     const signup = (user) => {
         console.log(user.name, user.email, user.password);
-        fetch(`${API}/signup`, {
+        // use return before fetch otherwise TypeError: Cannot read property 'then' of undefined
+        return fetch(`${API}/signup`, {
             method: "POST",
             headers: {
                 Accept: 'application/json',
@@ -39,26 +43,55 @@ const Signup = () => {
 
     const clickSubmit = (event) => {
         event.preventDefault();
-        signup({name, email, password});
+        setValues({...values, error: false});
+        signup({name, email, password})
+        .then(data => {
+            if(data.error) {
+                console.log(data.error);
+                setValues({...values, error: data.error, success: false})
+            } else {
+                setValues({
+                    ...values,
+                    name: '',
+                    email: '',
+                    password: '',
+                    error: '',
+                    success: true
+                })
+            }
+        })
     }
-
+    // component functions
     const signUpForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" />
+                <input onChange={handleChange('name')} type="text" className="form-control" value={name}/>
             </div>
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control" />
+                <input onChange={handleChange('email')} type="email" className="form-control" value={email}/>
             </div>
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control" />
+                <input onChange={handleChange('password')} type="password" className="form-control" value={password}/>
             </div>
             <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
         </form>
-    )
+    );
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-info" style={{display: success ? '' : 'none'}}>
+            New account is created. Please <Link to="/signin">Sign in</Link>
+        </div>
+    );
+
     // pass title, description and className into props to Layout 
     return (
         <Layout 
@@ -66,8 +99,9 @@ const Signup = () => {
             description="Signup to Node React E-Commerce App"
             className="container col-md-8 offset-md-2"
             >
+            {showSuccess()}
+            {showError()}
             {signUpForm()}
-            {JSON.stringify(values)}
         </Layout>
     )
 };
