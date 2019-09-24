7c85d82531d1373e1c3ebe75caa1df4f9138f470
Matthew Chick <c.chick@unsw.edu.au>
2019-09-24 11:18:18 +1000

    Create a signup form and get values from the form

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,13 +1,50 @@
 //Signup Component
-import React from 'react';
+import React, { useState } from 'react';    //use react hook
 import Layout from '../core/Layout';
 import { API } from '../config';
 
-const Signup = () => (
-    <Layout title="SignUp" description="Signup to Node React E-Commerce App">
-      
-        {API}
-    </Layout>
-);
+const Signup = () => {
+    // create a state
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input onChange={handleChange('email')} type="email" className="form-control" />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input onChange={handleChange('password')} type="password" className="form-control" />
+            </div>
+            <button className="btn btn-primary">Submit</button>
+        </form>
+    )
+    // pass title, description and className into props to Layout 
+    return (
+        <Layout 
+            title="SignUp" 
+            description="Signup to Node React E-Commerce App"
+            className="container col-md-8 offset-md-2"
+            >
+            {signUpForm()}
+            {JSON.stringify(values)}
+        </Layout>
+    )
+};
 
 export default Signup;
