411dc6733a0079e582c7906efec0675dda8557e2
Matthew Chick <c.chick@unsw.edu.au>
2019-09-25 13:52:32 +1000

    Create Admin Dashboard

--- src/user/AdminDashboard.js
+++ src/user/AdminDashboard.js
@@ -3,24 +3,24 @@ import Layout from '../core/Layout';
 import {Link} from 'react-router-dom';
 import { isAuthenticated } from '../auth/Auth';
 
-const Dashboard = () => {
+const AdminDashboard = () => {
     const {user: {_id, name, email, role}} = isAuthenticated();
-    const userLinks = () => {
+    const adminLinks = () => {
         return (
             <div className="card">
-                <h4 className="card-header">User Links</h4>
+                <h4 className="card-header">Admin Links</h4>
                 <ul className="list-group">
                     <li className="list-group-item">
-                        <Link className="nav-link" to= "/cart">My Cart</Link>
+                        <Link className="nav-link" to= "/create/category">Create Category</Link>
                     </li>
                     <li className="list-group-item">
-                        <Link className="nav-link" to= "/profile/update">Update Profile</Link>
+                        <Link className="nav-link" to= "/create/product">Create Product</Link>
                     </li>                   
                 </ul> 
             </div>
         )
     }
-    const userInfo = () => {
+    const AdminInfo = () => {
         return (
             <div className="card mb-5">
             <h3 className="card-header">User Information</h3>
@@ -32,28 +32,18 @@ const Dashboard = () => {
         </div>  
         )
     }
-    const purchaseHistory = () => {
-        return (
-            <div className="card mb-5">
-            <h3 className="card-header">Purchase history</h3>
-                <ul className="list-group">
-                    <li className="list-group-item">history</li>                   
-                </ul>           
-            </div> 
-        )
-    }
+    
     return (
         <Layout title="Dashboard" description={`G'day ${name}!`} className="container-fluid">   
             <div className="row">
                 <div className="col-3">
-                    {userLinks()}
+                    {AdminLinks()}
                 </div>
                 <div className="col-9">
-                    {userInfo()}
-                    {purchaseHistory()}
+                    {adminInfo()}
                 </div>         
             </div>   
         </Layout>
     );
 };
-export default Dashboard;
+export default AdminDashboard;
